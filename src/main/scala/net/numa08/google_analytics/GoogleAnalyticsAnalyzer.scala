877e904dea729f6bda936fd184c0bb672b8d6839
numa08 <n511287@gmail.com>
2014-09-23 12:12:57 +0900

    invoke analyzing by analytics

--- src/main/scala/net/numa08/google_analytics/GoogleAnalyticsAnalyzer.scala
+++ src/main/scala/net/numa08/google_analytics/GoogleAnalyticsAnalyzer.scala
@@ -1,35 +1,57 @@
 package net.numa08.google_analytics
 
-import scala.util.control.Exception._
-import scala.collection.JavaConversions._
-
 import java.util.Date
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.pattern.ask
+import akka.util.Timeout
 import com.google.api.services.analytics.Analytics
 import net.numa08.analyzer.{PVAnalyzer, PVAnalyzerResult}
+import net.numa08.google_analytics.Analyzer.Analyze
 import org.apache.commons.lang3.time.DateUtils
 
+import scala.collection.JavaConversions._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.util.control.Exception._
+
 class GoogleAnalyticsAnalyzer extends PVAnalyzer {
-  def analyze(identifiers : List[String]) : List[Either[Throwable, PVAnalyzerResult]] = ???
+  def analyze(identifiers : List[String]) : List[Either[Throwable, PVAnalyzerResult]] = {
+    val actorSystem = ActorSystem.create("googleanalytics")
+    val actor = actorSystem.actorOf(Props[Analyzer], "analyzer")
+    val analytics : Analytics = ???
+    implicit val timeout = Timeout(5 minutes)
+    val futures = identifiers.map{id =>
+      val message = Analyze(analytics, id)
+      (actor ? message).mapTo[Either[Throwable, PVAnalyzerResult]]
+    }
+    Await.result(Future.sequence(futures), timeout.duration)
+  }
 }
 
 sealed class Analyzer extends Actor with AnalyticsQuery {
   override def receive: Receive = {
-    case Analyze(analytics, id) => allCatch either {
-      val gaData = analytics.data().ga().get(queryById(id), startDateQuery, endDateQuery, metrics).execute()
-      val pv = gaData.getRows.head.head.toInt
-      val result = PVAnalyzerResult(pv, id)
+    case Analyze(analytics, id) => {
+      val result = allCatch either {
+        val gaData = analytics.data().ga().get(queryById(id), startDateQuery, endDateQuery, metrics).execute()
+        val pv = gaData.getRows.head.head.toInt
+        PVAnalyzerResult(pv, id)
+      }
       sender() ! result
     }
+
   }
-  case class Analyze(analytics : Analytics, id : String)
 
   override def startDate: Date = targetDate
 
   override def endDate: Date = targetDate
 
   private val targetDate = DateUtils.addDays(new Date(), -1)
+}
+
+object Analyzer {
+  case class Analyze(analytics : Analytics, id : String)
 }
 
 protected trait AnalyticsQuery {
