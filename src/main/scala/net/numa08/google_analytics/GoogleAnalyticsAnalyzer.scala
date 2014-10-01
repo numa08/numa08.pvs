b3ab5eadbaf100e904f0f810c4c7cf2af14fa646
numa08 <n511287@gmail.com>
2014-10-02 06:54:55 +0900

    invoke google analytics api
    but cannot authorization

--- src/main/scala/net/numa08/google_analytics/GoogleAnalyticsAnalyzer.scala
+++ src/main/scala/net/numa08/google_analytics/GoogleAnalyticsAnalyzer.scala
@@ -2,16 +2,37 @@ package net.numa08.google_analytics
 
 import scala.util.control.Exception._
 import scala.collection.JavaConversions._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 import java.util.Date
 
-import akka.actor.Actor
+import akka.actor.{Props, ActorSystem, Actor}
+import akka.pattern.ask
+import akka.util.Timeout
 import com.google.api.services.analytics.Analytics
 import net.numa08.analyzer.{PVAnalyzer, PVAnalyzerResult}
 import org.apache.commons.lang3.time.DateUtils
+import com.google.api.client.auth.oauth2.Credential
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.json.jackson2.JacksonFactory
+import net.numa08.google_analytics.Analyzer.Analyze
+import scala.concurrent.{Future, Await}
+
 
 class GoogleAnalyticsAnalyzer extends PVAnalyzer {
-  def analyze(identifiers : List[String]) : List[Either[Throwable, PVAnalyzerResult]] = ???
+  implicit val analyzeTimeout = Timeout(5 minutes)
+
+  def analyze(identifiers : List[String]) : List[Either[Throwable, PVAnalyzerResult]] = {
+    val credentials : Credential = ???
+    val transport = GoogleNetHttpTransport.newTrustedTransport
+    val jsonFactory = JacksonFactory.getDefaultInstance
+    val actorSystem = ActorSystem.create("google-analytics")
+    val actor = actorSystem.actorOf(Props[Analyzer], "analytics-analyzer")
+    val analytics = new Analytics.Builder(transport, jsonFactory, credentials).setApplicationName("analytics-analyzer").build()
+    val futures = identifiers.map{i => (actor ? Analyze(analytics, i)).mapTo[Either[Throwable, PVAnalyzerResult]]}
+    Await.result(Future.sequence(futures), analyzeTimeout.duration)
+  }
 }
 
 sealed class Analyzer extends Actor with AnalyticsQuery {
@@ -23,13 +44,17 @@ sealed class Analyzer extends Actor with AnalyticsQuery {
       sender() ! result
     }
   }
-  case class Analyze(analytics : Analytics, id : String)
+
 
   override def startDate: Date = targetDate
 
   override def endDate: Date = targetDate
 
   private val targetDate = DateUtils.addDays(new Date(), -1)
+}
+
+private object Analyzer {
+  case class Analyze(analytics : Analytics, id : String)
 }
 
 protected trait AnalyticsQuery {
