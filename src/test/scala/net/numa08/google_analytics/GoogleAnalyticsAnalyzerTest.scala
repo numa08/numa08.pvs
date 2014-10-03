b09e997cd6c6e97bdd8ec3326387ed64802469d7
numa08 <n511287@gmail.com>
2014-10-04 02:27:33 +0900

    add test

---
+++ src/test/scala/net/numa08/google_analytics/GoogleAnalyticsAnalyzerTest.scala
@@ -0,0 +1,10 @@
+package net.numa08.google_analytics
+
+import org.scalatest.{Matchers, FlatSpec}
+
+class GoogleAnalyticsAnalyzerTest  extends FlatSpec with Matchers{
+
+  "analyzer" should "be analyze" in {
+
+  }
+}
