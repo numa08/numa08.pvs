9c10399ae42a80d8caf44721eee7cb222398f25c
numa08 <n511287@gmail.com>
2014-10-06 11:37:51 +0900

    invoke google analytics test
    but failed because credential failed

--- src/test/scala/net/numa08/google_analytics/GoogleAnalyticsAnalyzerTest.scala
+++ src/test/scala/net/numa08/google_analytics/GoogleAnalyticsAnalyzerTest.scala
@@ -1,10 +1,16 @@
 package net.numa08.google_analytics
 
+import com.typesafe.config.ConfigFactory
+import net.numa08.analyzer.PVAnalyzerFactory
 import org.scalatest.{Matchers, FlatSpec}
 
 class GoogleAnalyticsAnalyzerTest  extends FlatSpec with Matchers{
 
   "analyzer" should "be analyze" in {
-
+    val analyzerName = "google-analytics"
+    val analyzer = PVAnalyzerFactory.analyzerByName(analyzerName)
+    val result = analyzer.analyze(ConfigFactory.load())
+    result.nonEmpty should be (true)
+    result.head.isRight should be (true)
   }
 }
